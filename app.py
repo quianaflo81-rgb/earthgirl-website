a1740e82a1e8309b64d1ecca3612d19f08c3dd56
QuianaCan <quianacan@berkeley.edu>
2026-05-30 18:10:21 -0700

    Initial commit - The Earth Girl Essentials website

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/shop")
+def shop():
+    return render_template("shop.html")
+
+@app.route("/experiences")
+def experiences():
+    return render_template("experiences.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
